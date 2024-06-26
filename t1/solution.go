a4fc6c621c0a27f31073f7536909a133ea23c23d
Nexadis <37233902+Nexadis@users.noreply.github.com>
2024-06-26 19:41:08 +0300

    t1

---
+++ t1/solution.go
@@ -0,0 +1,12 @@
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int, len(nums))
+	for i, n := range nums {
+		if j, ok := numMap[target-n]; ok {
+			return []int{i, j}
+		}
+		numMap[n] = i
+	}
+	return nil
+}
