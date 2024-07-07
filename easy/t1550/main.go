553e5352acfe9de170c3f9fa7b8443b195ddc34b
Nexadis <37233902+Nexadis@users.noreply.github.com>
2024-07-07 11:47:39 +0300

    t1550

---
+++ easy/t1550/main.go
@@ -0,0 +1,19 @@
+package main
+
+func threeConsecutiveOdds(arr []int) bool {
+	odds := 0
+	for i := 0; i < len(arr); i++ {
+		if odds == 3 {
+			return true
+		}
+		if arr[i]%2 == 0 {
+			odds = 0
+		} else {
+			odds++
+		}
+	}
+	if odds == 3 {
+		return true
+	}
+	return false
+}
